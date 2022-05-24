1463d360372ddd2c9e42ec6577efe5c50bd37867
yida <1147042405@qq.com>
2022-05-24 08:55:16 +0800

    NotesDatabaseHelper中PASSWORD表创建及相关结构修改

--- app/src/main/java/net/micode/notes/data/NotesDatabaseHelper.java
+++ app/src/main/java/net/micode/notes/data/NotesDatabaseHelper.java
@@ -84,7 +84,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
             "CREATE TABLE " + TABLE.PASSWROD + "(" +
                     PasswordColumns.ID + " INTEGER PRIMARY KEY," +
                     PasswordColumns.NOTE_ID + " INTEGER NOT NULL DEFAULT 0," +
-                    PasswordColumns.PASSWORD + " INTEGER NOT NULL DEFAULT 0," +
+                    PasswordColumns.PASSWORD + " INTEGER NOT NULL DEFAULT 0" +
                     ")";
 
     private static final String CREATE_DATA_NOTE_ID_INDEX_SQL =
